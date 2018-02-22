3bf94d65d2fea7eb12f92d953c30226bdf4b9893
Jay Foad <jay.foad@gmail.com>
2018-02-22 07:12:21 +0000

    Refactor to make it easy to call from Dyalog APL on various platforms.

--- incbeta.c
+++ incbeta.c
@@ -25,55 +25,74 @@
 
 
 #include <math.h>
+#include <stddef.h>
 
 #define STOP 1.0e-8
 #define TINY 1.0e-30
 
-double incbeta(double a, double b, double x) {
-    if (x < 0.0 || x > 1.0) return 1.0/0.0;
+void incbeta(double a, double b, double *p, size_t n)
+{
+	if (a <= 0 || b <= 0)
+	{
+		for (size_t i = 0; i < n; i++) p[i] = 0.0 / 0.0;
+		return;
+	}
 
-    /*The continued fraction converges nicely for x < (a+1)/(a+b+2)*/
-    if (x > (a+1.0)/(a+b+2.0)) {
-        return (1.0-incbeta(b,a,1.0-x)); /*Use the fact that beta is symmetrical.*/
-    }
+	/*Find the first part before the continued fraction.*/
+	const double lbeta_ab = lgamma(a) + lgamma(b) - lgamma(a + b);
 
-    /*Find the first part before the continued fraction.*/
-    const double lbeta_ab = lgamma(a)+lgamma(b)-lgamma(a+b);
-    const double front = exp(log(x)*a+log(1.0-x)*b-lbeta_ab) / a;
+	for (size_t i = 0; i < n; i++)
+	{
+		double x = p[i];
+		/*The continued fraction converges nicely for x < (a+1)/(a+b+2)*/
+		int swap = x > (a + 1) / (a + b + 2);
+		if (swap) { double t = a; a = b; b = t; x = 1 - x; } /*Use the fact that beta is symmetrical.*/
+		if (x <= 0)
+			x = 0;
+		else
+		{
+			const double front = exp(log(x) * a + log(1 - x) * b - lbeta_ab) / a;
 
-    /*Use Lentz's algorithm to evaluate the continued fraction.*/
-    double f = 1.0, c = 1.0, d = 0.0;
+			/*Use Lentz's algorithm to evaluate the continued fraction.*/
+			double f = 1, c = 1, d = 0;
+			for (int j = 0; ; j++)
+			{
+				int m = j / 2;
 
-    int i, m;
-    for (i = 0; i <= 200; ++i) {
-        m = i/2;
+				double numerator;
+				if (j == 0)
+					numerator = 1; /*First numerator is 1.0.*/
+				else if (j % 2 == 0)
+					numerator = m * (b - m) * x / ((a + 2 * m - 1) * (a + 2 * m)); /*Even term.*/
+				else
+					numerator = -(a + m) * (a + b + m) * x / ((a + 2 * m) * (a + 2 * m + 1));  /*Odd term.*/
 
-        double numerator;
-        if (i == 0) {
-            numerator = 1.0; /*First numerator is 1.0.*/
-        } else if (i % 2 == 0) {
-            numerator = (m*(b-m)*x)/((a+2.0*m-1.0)*(a+2.0*m)); /*Even term.*/
-        } else {
-            numerator = -((a+m)*(a+b+m)*x)/((a+2.0*m)*(a+2.0*m+1)); /*Odd term.*/
-        }
+				/*Do an iteration of Lentz's algorithm.*/
+				d = 1 + numerator * d;
+				if (fabs(d) < TINY) d = TINY;
+				d = 1 / d;
 
-        /*Do an iteration of Lentz's algorithm.*/
-        d = 1.0 + numerator * d;
-        if (fabs(d) < TINY) d = TINY;
-        d = 1.0 / d;
+				c = 1 + numerator / c;
+				if (fabs(c) < TINY) c = TINY;
 
-        c = 1.0 + numerator / c;
-        if (fabs(c) < TINY) c = TINY;
+				const double cd = c * d;
+				f *= cd;
 
-        const double cd = c*d;
-        f *= cd;
+				/*Check for stop.*/
+				if (fabs(1 - cd) < STOP)
+				{
+					x = front * (f - 1);
+					break;
+				}
 
-        /*Check for stop.*/
-        if (fabs(1.0-cd) < STOP) {
-            return front * (f-1.0);
-        }
-    }
-
-    return 1.0/0.0; /*Needed more loops, did not converge.*/
+				if (j >= 200)
+				{
+					x = 0.0 / 0.0; /*Needed more loops, did not converge.*/
+					break;
+				}
+			}
+		}
+		if (swap) { double t = a; a = b; b = t; x = 1 - x; }
+		p[i] = x;
+	}
 }
-
